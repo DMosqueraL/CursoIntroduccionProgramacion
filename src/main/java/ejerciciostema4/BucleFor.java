eda995bda35f87adde1d5fba3416463b6b03255c
DMosqueral <dml2410@gmail.com>
2022-11-19 15:24:30 -0500

    Bucle For

---
+++ src/main/java/ejerciciostema4/BucleFor.java
@@ -0,0 +1,17 @@
+package ejerciciostema4;
+
+/**
+ * Para el bucle For, crea una variable numeroFor, esta variable
+ * tendrá como valor 0 y su condición será que la variable sea igual
+ * o menor que 3, se irá incrementando en 1 su valor cada vez que se
+ * ejecute y deberá mostrarse por pantalla.
+ */
+public class BucleFor {
+    public static void main(String[] args) {
+        Integer numeroFor = 0;
+        for(int i = 0; i<= 3; i++){
+            numeroFor += 1;
+            System.out.println("Esto va: " +numeroFor);
+        }
+    }
+}
